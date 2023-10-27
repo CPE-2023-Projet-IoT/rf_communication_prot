a903f8e0d0d5b18bd596c76dded5fb41e457f8c0
Ruben CLERC <ruben.clerc@tpg08.cpe.lan>
2023-10-27 14:48:26 +0200

    Communication validée. TODO: check key

--- info_meteo_screen/main.cpp
+++ info_meteo_screen/main.cpp
@@ -2,13 +2,29 @@
 #include "bme280.h"
 #include "tsl256x.h"
 #include "ssd1306.h"
+#include "protocole.h"
+#include <string>
+#include <map>
 
 MicroBit uBit;
+
+// I2C
 MicroBitI2C i2c(I2C_SDA0,I2C_SCL0);
 MicroBitPin P1(MICROBIT_ID_IO_P1, MICROBIT_PIN_P1, PIN_CAPABILITY_DIGITAL_OUT);
 ssd1306 screen(&uBit, &i2c, &P1);
 
-void affichage_oled(bme280 bme, tsl256x tsl) {
+// Session management
+bool isSessionOk = false;
+ManagedString key2;
+
+void onData(MicroBitEvent) {
+    
+    key2 = uBit.radio.datagram.recv();
+    uBit.serial.printf("Key 2 received: %s\r\n", key2.toCharArray());
+    isSessionOk = true;
+}
+
+void display_rf_loop(bme280 bme, tsl256x tsl) {
 
     // INIT BME
     uint32_t pressure = 0;
@@ -29,33 +45,81 @@ void affichage_oled(bme280 bme, tsl256x tsl) {
 
     // Affichage BME
     ManagedString line = "Temp:" + ManagedString(tempe/100) + "." + ManagedString(tempe%100) +" C\r\n";
-    screen.display_line(4,0,line.toCharArray());
+    screen.display_line(1,0,line.toCharArray());
 
     line = "Humi:" + ManagedString(humi/100) + "." + ManagedString(humi%100)+" rH\r\n";
-    screen.display_line(5,0,line.toCharArray());
+    screen.display_line(2,0,line.toCharArray());
 
     line = "Press:" + ManagedString(press) +" hPa\r\n";
-    screen.display_line(6,0,line.toCharArray());
+    screen.display_line(3,0,line.toCharArray());
 
     // Affichage TSL
     line = "Lux:" + ManagedString((int)lux) + "\r\n";
-    screen.display_line(7,0,line.toCharArray());
+    screen.display_line(4,0,line.toCharArray());
 
-    // Update screen and sleep
+
+    // Send data
+    char temp = 'T';
+    char lum = 'L';
+    char hum = 'H';
+    char press = 'P';
+
+    std::string tempStr = to_string(tempe);
+    std::string lumStr = to_string(lux);
+    std::string pressStr = to_string(press);
+    std::string humStr = to_string(humi);
+
+    map<char, std::string> data;
+
+    data[temp] = tempStr;
+    data[lum] = lumStr;
+    data[press] = pressStr;
+    date[hum] = humStr;
+    uBit.serial.printf("Send data\r\n");
+    sendRf(&uBit,session, data);
+
+    // Update screen
     screen.update_screen();
-    uBit.sleep(500);
 }
 
 int main() {
 
     // Init Micro:bit et capteurs
     uBit.init();
+    uBit.radio.enable();
     bme280 bme(&uBit,&i2c);
     tsl256x tsl(&uBit,&i2c);
 
-    // Boucle d'affichage des données
-    while(true) {
-        affichage_oled(bme, tsl);
+    // Génère la clé
+    int key1 = keyGen(&uBit);
+    std::string key1Str = to_string(key1);
+    uBit.serial.printf("Key 1 generated: %d\r\n", key1);
+
+    uBit.radio.datagram.send(key1Str.c_str());
+    uBit.serial.printf("Key 1 sent\r\nWaiting for key 2...\r\n");
+
+    // Attend la clé pour initier la connection
+    uBit.messageBus.listen(MICROBIT_ID_RADIO, MICROBIT_RADIO_EVT_DATAGRAM, onData);
+
+    while(!isSessionOk) {
+        uBit.serial.printf("Still waiting...\r\n");
+        uBit.sleep(1000);
+    }
+
+    // Connection ok
+    uBit.serial.printf("Connection ok\r\n");
+    std::string key2Str(key2.toCharArray());
+    std::string session = computeKey(&uBit, key1Str, key2Str);
+    uBit.serial.printf("Session key: %s\r\n", session.c_str());
+
+    // Boucle de traitement
+    while(1) {
+
+        // Affichage
+        uBit.serial.printf("Refresh screen\r\n");
+        display_rf_loop(bme, tsl);
+
+        uBit.sleep(1000);
     }
 
     release_fiber();
