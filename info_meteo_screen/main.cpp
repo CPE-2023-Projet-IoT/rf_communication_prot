16cb388a41e4bae1ea6d4097ee4923c4473f9ad0
Ruben CLERC <ruben.clerc@tpg08.cpe.lan>
2023-10-27 14:17:34 +0200

    Modif affichage

--- info_meteo_screen/main.cpp
+++ info_meteo_screen/main.cpp
@@ -1,110 +1,62 @@
 #include "MicroBit.h"
 #include "bme280.h"
-#include "veml6070.h"
 #include "tsl256x.h"
 #include "ssd1306.h"
 
 MicroBit uBit;
 MicroBitI2C i2c(I2C_SDA0,I2C_SCL0);
-MicroBitPin P1(MICROBIT_ID_IO_P1, MICROBIT_PIN_P1, PIN_CAPABILITY_DIGITAL_OUT); //pin de reset
+MicroBitPin P1(MICROBIT_ID_IO_P1, MICROBIT_PIN_P1, PIN_CAPABILITY_DIGITAL_OUT);
 ssd1306 screen(&uBit, &i2c, &P1);
-int conf_affichage = 0;
 
-void onData(MicroBitEvent)
-{
-    PacketBuffer p = uBit.radio.datagram.recv();
-    int t[3];
-    memcpy(&t,p.getBytes(),3*sizeof(int));
-    
-    if(t[0]==7)
-    {
-        if(t[1]==2)
-        {
-            conf_affichage = t[2];  
-        }
-    }
-}
+void affichage_oled(bme280 bme, tsl256x tsl) {
 
-int main()
-{
-    int temp_2[7];
-
-    // Initialise the micro:bit runtime.
-    uBit.init();
-    bme280 bme(&uBit,&i2c);
+    // INIT BME
     uint32_t pressure = 0;
     int32_t temp = 0;
     uint16_t humidite = 0;
 
-    veml6070 veml(&uBit,&i2c);
-    uint16_t uv = 0;
-
-    tsl256x tsl(&uBit,&i2c);
+    // Init TSL
     uint16_t comb =0;
     uint16_t ir = 0;
     uint32_t lux = 0;
 
-    uBit.messageBus.listen(MICROBIT_ID_RADIO, MICROBIT_RADIO_EVT_DATAGRAM, onData);
-    uBit.radio.enable();
-    
+    // Récupération des valeurs
+    bme.sensor_read(&pressure, &temp, &humidite);
+    int tempe = bme.compensate_temperature(temp);
+    int press = bme.compensate_pressure(pressure)/100;
+    int humi = bme.compensate_humidity(humidite);
+    tsl.sensor_read(&comb, &ir, &lux);
 
-    while(true)
-    {
-        //BME
-        bme.sensor_read(&pressure, &temp, &humidite);
-        int tmp = bme.compensate_temperature(temp);
-        int pres = bme.compensate_pressure(pressure)/100;
-        int hum = bme.compensate_humidity(humidite);
-        veml.sensor_read(&uv);
-        tsl.sensor_read(&comb, &ir, &lux);
+    // Affichage BME
+    ManagedString line = "Temp:" + ManagedString(tempe/100) + "." + ManagedString(tempe%100) +" C\r\n";
+    screen.display_line(4,0,line.toCharArray());
 
-        temp_2[0]=25;        //change le protocole Mr CS2
-        temp_2[1]=1;
-        temp_2[2]=tmp;
-        temp_2[3]=pres;
-        temp_2[4]=hum;
-        temp_2[5]=uv;
-        temp_2[6]=lux;
+    line = "Humi:" + ManagedString(humi/100) + "." + ManagedString(humi%100)+" rH\r\n";
+    screen.display_line(5,0,line.toCharArray());
 
-        ManagedString display = "Temp:" + ManagedString(tmp/100) + "." + (tmp > 0 ? ManagedString(tmp%100): ManagedString((-tmp)%100))+" C\r\n";
-        //ManagedString display = "Temp:" + ManagedString(tmp);
-        screen.display_line(conf_affichage,0,display.toCharArray());
-        //uBit.serial.printf(display.toCharArray());
+    line = "Press:" + ManagedString(press) +" hPa\r\n";
+    screen.display_line(6,0,line.toCharArray());
 
-        display = "Humi:" + ManagedString(hum/100) + "." + ManagedString(tmp%100)+" rH\r\n";
-        //uBit.serial.printf(display.toCharArray());
-        screen.display_line(conf_affichage+1,0,display.toCharArray());
+    // Affichage TSL
+    line = "Lux:" + ManagedString((int)lux) + "\r\n";
+    screen.display_line(7,0,line.toCharArray());
 
-        display = "Pres:" + ManagedString(pres)+" hPa\r\n";
-        //uBit.serial.printf(display.toCharArray());
-        screen.display_line(conf_affichage+2,0,display.toCharArray());
+    // Update screen and sleep
+    screen.update_screen();
+    uBit.sleep(500);
+}
 
-        //VEML
-        
-        display = "UV:" + ManagedString(uv) + "\r\n";
-        //uBit.serial.printf(display.toCharArray());
-        screen.display_line(conf_affichage+3,0,display.toCharArray());
+int main() {
 
-        //TSL
+    // Init Micro:bit et capteurs
+    uBit.init();
+    bme280 bme(&uBit,&i2c);
+    tsl256x tsl(&uBit,&i2c);
 
-        
-        display = "Lux:" + ManagedString((int)lux) + "\r\n";
-        //uBit.serial.printf(display.toCharArray());
-        screen.display_line(conf_affichage+4,0,display.toCharArray());
-
-
-        display = "trame:" + ManagedString((int)conf_affichage)+ "\r\n";
-        screen.display_line(conf_affichage+5,0,display.toCharArray());
-        
-        PacketBuffer packet(7*sizeof(int));
-        memcpy(packet.getBytes(),temp_2,7*sizeof(int));
-        uBit.radio.datagram.send(packet);
-
-        screen.update_screen();
-
-        uBit.sleep(1000);
+    // Boucle d'affichage des données
+    while(true) {
+        affichage_oled(bme, tsl);
     }
-    release_fiber();
-    
 
+    release_fiber();
 }
